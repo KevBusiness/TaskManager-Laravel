6acf70cbd0a6cd30f436e7e89942a34aad53e44a
KevBusiness <kevinfelix.business@gmail.com>
2024-10-31 00:16:35 -0600

    added layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,9 @@
-<h1>The list of tasks</h1>
-<div>
+@extends('layouts.app')
+@section('title', 'The list of tasks')
+@section('content')
   @forelse ( $tasks as $task )
       <li>{{$task->title}}</li>
   @empty
     <div>There are no tasks!</div>
   @endforelse
-</div>
+@endsection
